1d7376658808b9900e1689e0d77d11e10513cc23
sungkwonkudo <sungkwonkudo@gmail.com>
2018-03-18 20:05:49 -0400

    made this more hydra oriented and simple

--- spring-boot-amqp-messaging/src/main/java/com/thepracticaldeveloper/rabbitmqconfig/CustomMessageSender.java
+++ spring-boot-amqp-messaging/src/main/java/com/thepracticaldeveloper/rabbitmqconfig/CustomMessageSender.java
@@ -2,7 +2,7 @@ package com.thepracticaldeveloper.rabbitmqconfig;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -14,12 +14,9 @@ public class CustomMessageSender {
 
     private static final Logger log = LoggerFactory.getLogger(CustomMessageSender.class);
 
-    private final RabbitTemplate rabbitTemplate;
-
     @Autowired
-    public CustomMessageSender(final RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
+    private AmqpTemplate rabbitTemplate;
+
 
     @Scheduled(fixedDelay = 3000L)
     public void sendMessage() {
--- spring-boot-amqp-receiver/src/main/java/com/thepracticaldeveloper/rabbitmqconfig/MessagingApplication.java
+++ spring-boot-amqp-receiver/src/main/java/com/thepracticaldeveloper/rabbitmqconfig/MessagingApplication.java
@@ -1,15 +1,10 @@
 package com.thepracticaldeveloper.rabbitmqconfig;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableRabbit
 @EnableScheduling
 public class MessagingApplication {
 
@@ -21,12 +16,4 @@ public class MessagingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingApplication.class, args);
 	}
-
-	// You can comment all methods below and remove interface's implementation to use the default serialization / deserialization
-	@Bean
-	public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
-		final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-		return rabbitTemplate;
-	}
-
 }
